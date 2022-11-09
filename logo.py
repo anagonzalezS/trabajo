5937ae7827672d5ec5f0c1fe97a56e4bc29b07a6
anagonzalezS <54723919+anagonzalezS@users.noreply.github.com>
2022-11-08 23:33:19 -0300

    Add files via upload

---
+++ logo.py
@@ -0,0 +1,28 @@
+
+
+def logo():
+	
+
+	
+	print("""  
+
+        ==============================================
+            BIENVENIDO A LA FUNDACION MAS POR MI PAIS
+        ==============================================
+         1 - Datos de la comuna
+         2 - Informe de la comuna
+         3 - Total de encuestadores 
+         4 - Comuna con mas encuestadores
+         5 - Comuna con mayor inflaccion
+         6 - Comuna con menor inflaccion 
+         7 - Informe ejecutivo
+         4 - Salir
+        ===============================================
+        """)
+logo()
+count = 0
+ans=True
+while ans:
+    ans= input("Seleccione la opccion deseada : ")
+	
+logo()
